22facc38cfad17ee5fd29822c360b862a51b7981
HummingBird1224 <oksanashuhaliia@gmail.com>
2023-10-01 14:06:36 +0900

    css repair again

--- client/src/layout/AuthLayout.js
+++ client/src/layout/AuthLayout.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import Header from '../components/Header/Header';
-import './Layout.css'
-import './../asset/css/fonts.css'
-import './../asset/css/sniper-theme.css'
+import './Layout.css';
 
 
 const AuthLayout = (props) => {
@@ -11,8 +9,8 @@ const AuthLayout = (props) => {
   const login = path.includes('login');
   return (
     <React.Fragment>
-      {login&&<Header />}
-      <div className={ login ? 'auth__page' : '' }>
+      {login && <Header />}
+      <div className={login ? 'auth__page' : ''}>
         {props.children}
       </div>
     </React.Fragment>
