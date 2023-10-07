7c27ec31f274c32619874d13b3fca468c74a90a7
HummingBird1224 <oksanashuhaliia@gmail.com>
2023-10-07 18:56:01 +0900

    fatty change

--- client/src/components/Feature/CheckBox.jsx
+++ client/src/components/Feature/CheckBox.jsx
@@ -1,12 +1,20 @@
-const CheckBox = ({ props }) => {
-    return (
-        <div className="form--type_checkbox_group">
-            <input type="checkbox" className="formPart" id={"checkbox_4iZknn7ZsCDpQP5gt_" + props.value} name="6_4iZknn7ZsCDpQP5gt" value={props.value} />
-            <label for={"checkbox_4iZknn7ZsCDpQP5gt_" + props.value}>
-                <span>{props.text}</span>
-            </label>
-        </div>
-    );
+import { answer } from '../../actions/action.js';
+
+function PartTime({ props }) {
+  const handleClick = (event) => {
+    var { name, value } = event.target;
+    console.log('this is the console from the es6 syntax', name, value);
+    answer();
+  };
+
+  return (
+    <div className="form--type_checkbox_group">
+      <input type="checkbox" className="formPart" id={"insurance_type" + props.value} name="insurance_type" value={props.value} onClick={ handleClick }/>
+      <label htmlFor={"insurance_type" + props.value}>
+        <span>{props.text}</span>
+      </label>
+    </div> 
+  );
 };
 
-export default CheckBox;
+export default PartTime;
